5d04d4e0edaaa8852dcfc8d2734d273633047be0
Roseokpe <roseokpe@gmail.com>
2022-09-28 11:35:36 -0700

    Added nameable class, trimmer and capitalize class

--- classes/person.rb
+++ classes/person.rb
@@ -1,11 +1,15 @@
-class Person
+require_relative 'nameable'
+require_relative 'base_decor'
+
+class Person < Nameable
   attr_accessor :name, :age, :id, :parent_permission
 
   def initialize(age, name = 'unknown', parent_permission: true)
-    @id = Random rand(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
@@ -24,4 +28,15 @@ class Person
     end
   end
   private :of_age?
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
