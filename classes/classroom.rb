58824d80995cf652a02a91d82891be8000be6257
Roseokpe <roseokpe@gmail.com>
2022-09-29 10:59:51 -0700

    add set and add student to add_student method

--- classes/classroom.rb
+++ classes/classroom.rb
@@ -5,7 +5,9 @@ class Classroom
         @label = label 
         @student = []
     end
-    def add_student
-        @student.push(student)
+    def add_student(student)
+        @student.push(student) # to add student to a classroom
+        #to check if the student is not already belong to a classroom  # to set
+        student.classroom = self unless student.classroom = self
     end
 end
