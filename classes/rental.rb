714e77b0337bc0ef2884763d638093547a6cd11a
Roseokpe <roseokpe@gmail.com>
2022-09-29 12:59:39 -0700

    fix linters errors

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,11 +1,11 @@
 class Rental
-    attr_accessor :data, :person, :book
-    def initialize(date, person, book)
-        @date = date 
-        @person = person
-        @person.rental.push(self)
-        @book = book
-        @book.rental.push(self)
-        
-    end
+  attr_accessor :data, :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    @person.rental.push(self)
+    @book = book
+    @book.rental.push(self)
+  end
 end
